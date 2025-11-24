e806df029a7e4f4698d42b24dee8cb067c2f1732
rawanst <rawantabaja12@gmail.com>
2025-11-24 10:59:19 +0100

    OK: Create DataHelper class

---
+++ Utility/DataHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using portfolio_api.Data;
+
+namespace portfolio_api.Utility
+{
+    public class DataHelper
+    {
+        public static async Task ManageDataAsync(IServiceProvider svcProvider)
+        {
+            // Service: An instance od db context
+            var dbContextSvc = svcProvider.GetRequiredService<ApplicationDbContext>();
+
+            // Migration: This is the programmatic equivalent to Update-Database
+            await dbContextSvc.Database.MigrateAsync();
+        }
+    }
+}
